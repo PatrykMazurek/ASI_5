e036688182306f5b3725734a33b72cf5ef82f54d
PatrykMazurek <33990246+PatrykMazurek@users.noreply.github.com>
2023-11-26 18:19:36 +0100

    Projekt dla grupy ASI 5

---
+++ src/main/java/com/example/asi5/Asi5Application.java
@@ -0,0 +1,13 @@
+package com.example.asi5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Asi5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Asi5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/asi5/Asi5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asi5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Asi5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
